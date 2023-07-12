4a3e9c584d2e95ce1b0ac71a2ca9c8557499556b
Connor Bluestein <cbluestein@vt.edu>
2023-07-12 12:58:51 -0400

    added get_prefixes

--- database_manager.py
+++ database_manager.py
@@ -1,15 +1,37 @@
-def get_asn(ip)
+from pymongo import MongoClient
 
-def get_prefixes(asn)
+# Administrative Actions
 
-def register_asn(asn)
+def get_asn(collection, ip):
+    data = collection.find({"prefix/masks.ip":ip}, {"_id":0, "asn":1})
+    all_asn = data[0]["asn"]
+    
 
-def register_prefixes(asn, ip)
+def get_prefixes(collection, asn):
+    data = collection.find({"public_key":asn}, {"_id":0, "prefix/masks":1})
+    all_prefixes = data[0]["prefix/masks"]
+    prefixes = [prefix["ip"] for prefix in all_prefixes]
+    return prefixes
 
-def remove_asn(asn)
+def register_asn(collection, asn):
+    pass
 
-def remove_prefix(asn, ip)
+def register_prefixes(collection, asn, ip):
+    pass
 
-def request_contact(ip)
+def remove_asn(collection, asn):
+    pass
 
-def register_contact(ip)
+def remove_prefix(collection, asn, ip):
+    pass
+
+def request_contact(collection, ip):
+    data = collection.find({"ip":ip})
+
+def register_contact(collection, ip):
+    pass
+
+client = MongoClient('127.21.0.2', 27017, username='root', password='root')
+db = client["my_database"]
+collection = db["ASN"]
+

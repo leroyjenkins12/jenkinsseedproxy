d42000ffd6366484b0da7fcc18ecbb70d421ff00
Connor Bluestein <cbluestein@vt.edu>
2023-07-20 09:48:39 -0400

    Trying to install mongodb

--- Seed_Autoscale/examples/1to1_20.py
+++ Seed_Autoscale/examples/1to1_20.py
@@ -31,7 +31,7 @@ ovpn    = OpenVpnRemoteAccessProvider()
 
 
 ###############################################################################
-ix81 = base.createInternetExchange(81)
+ix100 = base.createInternetExchange(100)
 ix2 = base.createInternetExchange(2)
 ix3 = base.createInternetExchange(3)
 ix4 = base.createInternetExchange(4)
@@ -81,11 +81,11 @@ else: # no percentage specified, do not deploy proxy
 #None in this toplogy
 
 ## Tier 2 ASes: QTY - 8
-Makers.makeTransitAs(base, 85, [2, 81], [(2, 81)], proxy[0])
+Makers.makeTransitAs(base, 85, [2, 100], [(2, 100)], proxy[0])
 Makers.makeTransitAs(base, 86, [2, 11], [(2, 11)], proxy[1])
 Makers.makeTransitAs(base, 91, [12, 20], [(12, 20)], proxy[2])
 Makers.makeTransitAs(base, 92, [6, 12], [(6, 12)], proxy[3])
-Makers.makeTransitAs(base, 93, [12, 81], [(12, 81)], proxy[4])
+Makers.makeTransitAs(base, 93, [12, 100], [(12, 100)], proxy[4])
 Makers.makeTransitAs(base, 94, [12, 3], [(12, 3)], proxy[5])
 Makers.makeTransitAs(base, 95, [17, 10], [(17, 10)], proxy[6])
 Makers.makeTransitAs(base, 110, [10, 2], [(10, 2)], proxy[7])
@@ -95,7 +95,7 @@ Makers.makeTransitAs(base, 114, [8, 5], [(8, 5)], proxy[10])
 Makers.makeTransitAs(base, 115, [17, 16], [(17, 16)], proxy[11])
 Makers.makeTransitAs(base, 116, [16, 8], [(16, 8)], proxy[12])
 Makers.makeTransitAs(base, 135, [8, 15], [(8, 15)], proxy[14])
-Makers.makeTransitAs(base, 136, [81, 4], [(81, 4)], proxy[15])
+Makers.makeTransitAs(base, 136, [100, 4], [(100, 4)], proxy[15])
 Makers.makeTransitAs(base, 137, [7, 14], [(7, 14)], proxy[16])
 Makers.makeTransitAs(base, 138, [4, 14], [(4, 14)], proxy[17])
 Makers.makeTransitAs(base, 153, [7, 19], [(7, 19)], proxy[18])
@@ -104,7 +104,7 @@ Makers.makeTransitAs(base, 154, [13, 3], [(13, 3)], proxy[19])
 
 #Makers.makeTransitAs(base, 164, [187, 188], [(187, 188)], proxy[79])
 #####################################################################
-Makers.makeStubAs(emu, base, 249, 81, [None], proxy[20])
+Makers.makeStubAs(emu, base, 249, 100, [None], proxy[20])
 Makers.makeStubAs(emu, base, 170, 2, [None], proxy[21])
 Makers.makeStubAs(emu, base, 171, 3, [None], proxy[22])
 Makers.makeStubAs(emu, base, 172, 4, [None], proxy[23])
@@ -146,7 +146,7 @@ as11872.createRealWorldRouter('rw').joinNetwork('ix2', '10.2.0.118')
 # None of them will provide transit service for others. 
 
 
-ebgp.addRsPeers(81, [249, 85, 136, 93])
+ebgp.addRsPeers(100, [249, 85, 136, 93])
 ebgp.addRsPeers(2, [170, 85, 86, 11872])
 ebgp.addRsPeers(3, [171, 94])
 ebgp.addRsPeers(4, [172, 136,138])
@@ -160,7 +160,7 @@ ebgp.addRsPeers(11, [179, 86])
 ebgp.addRsPeers(12, [180, 91, 92, 94, 93])
 ebgp.addRsPeers(13, [181, 154])
 ebgp.addRsPeers(14, [182, 137, 138])
-ebgp.addRsPeers(15, [183, 135, 134])
+ebgp.addRsPeers(15, [183, 135])
 ebgp.addRsPeers(16, [184, 115, 116])
 ebgp.addRsPeers(17, [185, 113, 115, 95])
 ebgp.addRsPeers(18, [186, 112])
@@ -191,4 +191,3 @@ emu.dump('base-component.bin')
 # Uncomment the following if you want to generate the final emulation files
 emu.render()
 emu.compile(Docker(), './output_1to1_20', override=True)
-
---
+++ packet_proxy.py
@@ -0,0 +1,90 @@
+from scapy.all import *
+from scapy.contrib.bgp import *
+from pymongo import MongoClient
+
+
+def check_exists(asn, ip):
+    # Create a MongoClient instance
+    client = MongoClient('10.100.0.0', 27017, username='root', password='root')
+    db = client.data.ROOT
+
+    result = list(db.find({'prefix/masks.ip': ip}))
+
+    is_found = False
+    
+    for entry in result:
+        if entry["asn"] == asn:
+            is_found = True
+            break
+
+    # Close the connection
+    client.close()
+
+    return is_found
+
+# Iterate over the packets
+def proxy(packet):
+    print(packet.summary())
+    if BGPUpdate in packet:
+        seen = []
+        counter = 0
+        while True:
+            bgp_update = packet.getlayer(counter)
+            if bgp_update is None:
+                # print("end of packet at", counter)
+                break
+            counter += 1
+
+            try:
+                if hasattr(bgp_update, 'path_attr'):
+                    path_attributes = bgp_update.path_attr
+                    # for path_entry in path_attributes:
+                    #     type_code = path_entry.type_code
+                    #     if type_code == 2:
+                    #         asn = path_entry.attribute.segments[0].segment_value[-1]
+                    #         for count, nlri in enumerate(bgp_update.nlri):
+                    #             ip = str(nlri.prefix)
+                    #             if ip in seen:
+                    #                 print(f"On layer {counter} repeated: {asn} {ip}")
+                    #             else:
+                    #                 print(f"On layer {counter}: {asn} {ip}")
+                    #                 seen.append(ip)
+                    #             # print(check_exists(asn, ip))
+                    #     else:e66aecc9db2b
+                    #         print(f"On layer {counter}: Type code error: {type_code}")
+
+                    path_entry = path_attributes[1]
+                    type_code = path_entry.type_code
+                    if type_code == 2:
+                        asn = path_entry.attribute.segments[0].segment_value[-1]
+                        for count, nlri in enumerate(bgp_update.nlri):
+                            ip = str(nlri.prefix)
+                            if ip in seen:
+                                # print(f"On layer {counter} repeated: {asn} {ip}")
+                                pass
+                            else:
+                                print(f"On layer {counter}: {asn} {ip}")
+                                seen.append(ip)
+                                print(check_exists(asn, ip))
+                    else:
+                        # print(f"On layer {counter}: Type code error: {type_code}")
+                        pass
+                            
+                else:
+                    # print(f"On layer {counter}: No attributes, breaking")
+                    break
+            except:
+                pass
+    else:
+        print("BGP Update not found in packet")
+        pass
+            
+# tcpdump -i any port 179 -w example.pcap
+try:
+    sniff(prn=proxy, filter="port 179", iface="ix100")
+    # sniff(prn=lambda x:x.summary(), filter="port 179")
+except KeyboardInterrupt:
+    pass
+
+# from scapy.all import *
+# sniff(prn=lambda x:x.summary(), filter="port 179", iface="ix12")

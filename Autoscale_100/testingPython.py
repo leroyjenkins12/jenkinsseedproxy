369e06a55a7750e51df2cb1671c0cf25133fd76e
Connor Bluestein <nthn.jnkns@icloud.com>
2023-08-09 13:16:52 -0400

    playing with pymongo

--- Autoscale_100/testingPython.py
+++ Autoscale_100/testingPython.py
@@ -1,4 +1,7 @@
-# from pymongo import MongoClient
+from pymongo import MongoClient
 # from random import randint
+client = MongoClient('10.12.0.12', 27017)
+# mydb = client["bgp_db"]
+
 with open('testing123.txt', 'w') as dummy:
-    dummy.write("foo")
+    dummy.write(client.list_database_names())
